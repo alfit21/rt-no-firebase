35f5e8210bc30b44f8d4d58b087f2c29976f14af
alfit <muhammadalfit808@gmail.com>
2021-12-16 13:39:47 +0700

    membenarkan util

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,5 +1,5 @@
 abstract class Controllers {
   static final url = 'http://10.1.11.107:80/';
   // static final url = 'https://b3fe-180-252-161-14.ngrok.io/';
-  // $ php artisan serve --host 10.1.11.107 --port 80
+  // $ php spark serve --host 10.1.11.107 --port 80
 }
